b89bffb5fca90d7f44fd9ac35a462a0d6be8aa7d
vendisaputra <vendisaputra@gmail.com>
2022-09-30 10:39:14 +0700

    Materi Date Parsing Jackson

---
+++ src/main/java/com/sinaukoding/tokosinau/common/DateDeserialize.java
@@ -0,0 +1,41 @@
+package com.sinaukoding.tokosinau.common;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@JsonComponent
+public class DateDeserialize extends JsonDeserializer<Date> {
+
+    private static final String[] DATE_FORMAT = {
+            "dd-MM-yyyy",
+            "dd/MM/yyyy",
+            "dd.MM.yyyy"
+    };
+
+    @Override
+    public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        if (jsonParser == null || jsonParser.getText().equals("")){
+            return null;
+        }
+
+        String date = jsonParser.getText();
+
+        for (String format : DATE_FORMAT) {
+            try {
+                return new SimpleDateFormat(format).parse(date);
+            }catch (ParseException e){
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
